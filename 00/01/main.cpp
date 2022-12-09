30d376f69cfeb5073644b41d762255cfd42afba7
Marco Sebastiani <masebast@e4r3p3.42roma.it>
2022-12-09 20:20:05 +0100

    update

---
+++ 00/01/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 16:55:03 by masebast          #+#    #+#             */
+/*   Updated: 2022/12/09 17:23:51 by masebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.class.hpp"
+#include "Phonebook.class.hpp"
+
+int main()
+{
+	std::string cmd;
+	Phonebook phone_book;
+	while (1)
+	{
+		
+	}
+	return (0);
+}
