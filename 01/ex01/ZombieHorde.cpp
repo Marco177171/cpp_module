e50097e454c63ea9ef978758a2b6121da6a60621
Marco177171 <msebastiani93@gmail.com>
2022-12-22 09:24:42 +0100

    update

---
+++ 01/ex01/ZombieHorde.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+Zombie *zombieHorde(int N, std::string name)
+{
+	Zombie *arr;
+	int i;
+
+	arr = new Zombie[N];
+	i = 0;
+	while (i < N)
+	{
+		arr[i++].setName(name);
+		std::cout << "zombieHorde function finished" << std::endl;
+		return (arr);
+	}
+}
