c235330bac02841d98c90d431a9ef699aa25acf2
Marco Sebastiani <masebast@e3r3p8.42roma.it>
2023-01-09 17:14:14 +0100

    update

--- Module01/ex06/main.cpp
+++ Module01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:58:27 by masebast          #+#    #+#             */
-/*   Updated: 2023/01/09 16:58:47 by masebast         ###   ########.fr       */
+/*   Updated: 2023/01/09 17:13:24 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ void HarlFilter(std::string level, Harl *harl)
 	int type;
 
 	type = 0;
-	if (level.compare("debug"))
+	if (level.compare("debug") == 0)
 		type = 1;
-	else if (level.compare("info"))
+	else if (level.compare("info") == 0)
 		type = 2;
-	else if (level.compare("warning") == 1)
+	else if (level.compare("warning") == 0)
 		type = 3;
-	else if (level.compare("error"))
+	else if (level.compare("error") == 0)
 		type = 4;
 	switch (type)
 	{
@@ -57,7 +57,10 @@ int main(int argc, char *argv[])
 	Harl *harl = new Harl();
 
 	if (argc != 2)
+	{
+		delete harl;
 		return (ft_error("Error: arguments must be exactly two"));
+	}
 	level = argv[1];
 	HarlFilter(level, harl);
 	delete harl;
