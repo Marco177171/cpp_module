8aeb7d4445808a9f9830bfdc972906df55ddff2f
Marco Sebastiani <masebast@e3r1p6.42roma.it>
2023-01-31 19:20:04 +0100

    M4E3

---
+++ Module04/ex03/AMateria.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 19:13:40 by masebast          #+#    #+#             */
+/*   Updated: 2023/01/31 19:19:07 by masebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+# include <iostream>
+
+class AMateria
+{
+	public:
+		AMateria(void);
+		AMateria(const AMateria &source);
+		AMateria(std::string const &type);
+		AMateria &operator=(const AMateria &source);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter &target);
+	protected:
+};
+
+#endif
