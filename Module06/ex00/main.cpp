532cff74d176e6faf6c47d986eadd05e30d3dc6c
Marco Sebastiani <masebast@e3r9p1.42roma.it>
2023-02-10 17:49:54 +0100

    M6E0 gone

--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 16:16:56 by masebast          #+#    #+#             */
-/*   Updated: 2023/02/10 16:51:15 by masebast         ###   ########.fr       */
+/*   Updated: 2023/02/10 17:48:36 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,11 +43,45 @@ int InfOrNanConversion(const char* nToConvert)
 	return (0);
 }
 
+int normalConversion(std::string str)
+{
+	double converted;
+
+	if (std::isalpha(str[0]) && !str[1])
+		converted = static_cast<char>(str[0]);
+	else if (std::isalpha(str[0] && str[0]))
+		return (error("Not a valid parameter"));
+	else
+		converted = std::strtold(str.c_str(), NULL);
+	if (converted < CHAR_MIN || converted > CHAR_MAX)
+		std::cout << "char: impossible" << std::endl;
+	else if (!std::isprint(converted))
+		std::cout << "char: not displayable" << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(converted) << "'" << std::endl;
+	if (converted < INT_MIN || converted > INT_MAX)
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << static_cast<int>(converted) << std::endl;
+	if (converted < -FLT_MAX || converted > FLT_MAX)
+		std::cout << "float: impossible" << std::endl;
+	else
+		std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(converted) << "f" << std::endl;
+	if (converted < -DBL_MAX || converted > DBL_MAX)
+		std::cout << "double: impossible" << std::endl;
+	else if (std::isinf(converted))
+		std::cout << "double: inf" << std::endl;
+	else
+		std::cout << "double: " << static_cast<double>(converted) << std::endl;
+	return (0);
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 		return (error("Error: I have to take exactly one argument"));
 	if (checkInfOrNan(argv[1]))
 		return (InfOrNanConversion(checkInfOrNan(argv[1])));
-	return (0);
+	else
+		return (normalConversion(argv[1]));
 }
