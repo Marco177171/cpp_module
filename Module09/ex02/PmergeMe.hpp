f3a2724b9ab9b4206abbbd6f86fb213468eb7558
Marco Sebastiani <masebast@e3r5p3.42roma.it>
2023-04-28 18:45:48 +0200

    update | to do: check ex00 + write ex02

--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -6,16 +6,13 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:07:31 by masebast          #+#    #+#             */
-/*   Updated: 2023/04/24 18:08:15 by masebast         ###   ########.fr       */
+/*   Updated: 2023/04/28 17:13:22 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
-class PMERGEME_HPP {
-    public:
-    private:
-};
+# include <iostream>
 
 #endif
