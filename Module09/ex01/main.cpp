4c6f24907680397b3a96c76e1f7f3741b9199fe8
Marco Sebastiani <masebast@e3r5p3.42roma.it>
2023-04-28 17:11:59 +0200

    cpp09ex01 gone

--- Module09/ex01/main.cpp
+++ Module09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:04:21 by masebast          #+#    #+#             */
-/*   Updated: 2023/04/27 19:02:35 by masebast         ###   ########.fr       */
+/*   Updated: 2023/04/28 17:02:26 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,66 @@ int checkArgs(char *argv)
 	i = 0;
 	while (argv[i])
 	{
-		if (argv[i] != '+' && argv[i] != '-' && argv[i] != '/' && argv[i] != '(' &&
-			argv[i] != ')' && argv[i] != ' ' && argv[i] != '*' && !isnumber(argv[i]))
+		if (argv[i] != '+' && argv[i] != '-' && argv[i] != '/' &&
+			argv[i] != ' ' && argv[i] != '*' && !std::isdigit(argv[i]))
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-int parse(char *argv)
+int operate(std::stack<int> *stack, char operand)
 {
-	(void)argv;
-	std::cout << "parsing.." << std::endl;
+	int n1, n2, result;
+
+	result = 0;
+	n2 = stack->top();
+	stack->pop();
+	n1 = stack->top();
+	stack->pop();
+	if (operand == '+')
+		result = n1 + n2;
+	else if (operand == '-')
+		result = n1 - n2;
+	else if (operand == '*')
+		result = n1 * n2;
+	else if (operand == '/')
+		result = n1 / n2;
+	else
+		return (ft_error("ERROR: unexpected token found among operands"));
+	stack->push(result);
+	return (0);
+}
+
+int parse(char *argv, std::stack<int> *stack)
+{
+	int index;
+
+	index = 0;
+	while (argv[index])
+	{
+		if (std::isdigit(argv[index]))
+			stack->push(argv[index] - '0');
+		else if (argv[index] == '+' || argv[index] == '-' ||
+			argv[index] == '*' || argv[index] == '/')
+			operate(stack, argv[index]);
+		index++;
+	}
 	return (0);
 }
 
 int main(int argc, char *argv[])
 {
+	std::stack<int> stack;
+
 	if (argc != 2)
 		return (ft_error("ERROR: argument"));
 	if (checkArgs(argv[1]))
 		return (ft_error("ERROR: invalid arguments"));
-	parse(argv[1]);
+	parse(argv[1], &stack);
+	if (stack.size() == 1)
+		std::cout << stack.top() << std::endl;
+	else
+		return (ft_error("ERROR in notation syntax"));
 	return (0);
 }
--- Module09/ex01/rpn.hpp
+++ Module09/ex01/rpn.hpp
@@ -6,19 +6,14 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:06:39 by masebast          #+#    #+#             */
-/*   Updated: 2023/04/27 18:32:59 by masebast         ###   ########.fr       */
+/*   Updated: 2023/04/28 16:35:35 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 # define RPN_HPP
 
-#include <iostream>
-
-class RPN {
-    public:
-        
-    private:
-};
+# include <iostream>
+# include <stack>
 
 #endif
