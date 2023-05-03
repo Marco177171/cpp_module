8bea89bf8aafa0d2febb3e18c2d81e246149415e
Marco Sebastiani <masebast@e3r2p6.42roma.it>
2023-05-03 18:20:35 +0200

    aaaAAAaaaAaaAa

--- Module09/ex02/List.cpp
+++ Module09/ex02/List.cpp
@@ -6,7 +6,7 @@
 /*   By: masebast <masebast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:04:50 by masebast          #+#    #+#             */
-/*   Updated: 2023/05/02 18:43:33 by masebast         ###   ########.fr       */
+/*   Updated: 2023/05/03 18:20:14 by masebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,76 @@ int fillList(std::list<int> *mergeTarget, char *argv[])
 	return (0);
 }
 
+int myMerge(std::list<int> *mergeTarget, int begin, int end)
+{
+	(void) begin;
+	(void) end;
+	int counter = 0;
+	int average = mergeTarget->size() / 2;
+	std::list<int> left;
+	std::list<int> right;
+	std::list<int>::iterator iter;
+	std::list<int>::iterator leftIter;
+	std::list<int>::iterator rightIter;
+
+	iter = mergeTarget->begin();
+	// create left and right
+	while (iter != mergeTarget->end())
+	{
+		if (counter < average)
+			left.push_back(*iter);
+		else
+			right.push_back(*iter);
+		counter++;
+		iter++;
+	}
+	// sort list
+	iter = mergeTarget->begin();
+	leftIter = left.begin();
+	while (leftIter != left.end())
+	{
+		rightIter = right.begin();
+		while (rightIter != right.end())
+		{
+			if (*leftIter > *rightIter)
+			{
+				mergeTarget->insert(iter, *rightIter, 2);
+				iter++;
+				break;
+			}
+			rightIter++;
+		}
+		leftIter++;
+	}
+	return (0);
+}
+
+int mySort(std::list<int> *mergeTarget, int begin, int end)
+{
+	int average = (end + begin) / 2;
+
+	if (begin - end > 10)
+	{
+		mySort(mergeTarget, begin, average);
+		mySort(mergeTarget, average, end);
+		myMerge(mergeTarget, begin, end);
+	}
+	// else
+	// {
+	// 	myInsertSort();
+	// }
+	return (0);
+}
+
 int sortAndMergeList(std::list<int> *mergeTarget)
 {
-	mergeTarget->sort();
-	mergeTarget->unique();
+	int begin, end;
+
+	begin = 0;
+	end = mergeTarget->size();
+	mySort(mergeTarget, begin, end);
+	// mergeTarget->sort();
+	// mergeTarget->unique();
 	return (0);
 }
 
